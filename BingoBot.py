12f4dd586f56f3f52009a83580b349319319aa5b
BaneJammin <35785950+BaneJammin@users.noreply.github.com>
2019-06-05 16:58:51 -0400

    Update BingoBot.py
    Learning decorators to improve DRY

--- BingoBot.py
+++ BingoBot.py
@@ -3,11 +3,34 @@ import random
 import BingoBotConfig
 
 access_token = BingoBotConfig.access_token
-##yammer = yampy.Yammer(access_token=access_token)
+#yammer = yampy.Yammer(access_token=access_token)
+already_picked = {column:[] for column in 'BINGO'}
 
-already_called = []
+def check_ball(func):
+    print('Check ball')
+    def wrapper(d):
+        print('Wrapper')
+        func_res = func()
+        print('Wrapper return')
+        return func_res[1] in d[func_res[0]]
+    print('Check ball return')
+    return wrapper
 
-def pick_number():
-    picked = random.randint(1, 75)
-    return f'{"BINGO"[(picked - 1) // 15]}{picked}'
+@check_ball
+def pick_ball(min=1, max=5):
+    print('Pick ball')
+    picked_number = random.randint(7,10)
+    picked_letter = f'{"BINGO"[(picked_number - 1) // 15]}'
+    print(picked_letter, picked_number)
+    print('Pick ball return')
+    return (picked_letter, picked_number)
 
+while not pick_ball(already_picked):
+    print('False, repicking...')
+
+    
+
+def print_drawn(dict):
+    print('Called numbers:')
+    for i in dict:
+        print(f'{i}: {dict[i]}')
