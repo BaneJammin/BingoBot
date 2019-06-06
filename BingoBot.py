b57b0a560492deea4ce989d921fe3d338e56dd6d
BaneJammin <35785950+BaneJammin@users.noreply.github.com>
2019-06-06 10:04:16 -0400

    Update BingoBot.py
    Backtracking the over-engineered decorator route

--- BingoBot.py
+++ BingoBot.py
@@ -4,33 +4,28 @@ import BingoBotConfig
 
 access_token = BingoBotConfig.access_token
 #yammer = yampy.Yammer(access_token=access_token)
-already_picked = {column:[] for column in 'BINGO'}
+picked = {column:[] for column in 'BINGO'}
 
-def check_ball(func):
-    print('Check ball')
-    def wrapper(d):
-        print('Wrapper')
-        func_res = func()
-        print('Wrapper return')
-        return func_res[1] in d[func_res[0]]
-    print('Check ball return')
-    return wrapper
+def pick():
+    number = random.randint(1,75)
+    letter = f'{"BINGO"[(number - 1) // 15]}'
+    return (letter, number)
 
-@check_ball
-def pick_ball(min=1, max=5):
-    print('Pick ball')
-    picked_number = random.randint(7,10)
-    picked_letter = f'{"BINGO"[(picked_number - 1) // 15]}'
-    print(picked_letter, picked_number)
-    print('Pick ball return')
-    return (picked_letter, picked_number)
+def new_ball(dict=picked):
+    letter, number = pick()    
+    while number in dict[letter]:
+        print(f'{letter} {number}: Repicking...')
+        letter, number = pick()
+    dict[letter].append(number)
+    return (letter, number)
 
-while not pick_ball(already_picked):
-    print('False, repicking...')
-
-    
-
-def print_drawn(dict):
+def print_drawn(dict=picked):
     print('Called numbers:')
     for i in dict:
-        print(f'{i}: {dict[i]}')
+        print(f'{i}: {sorted(dict[i])}')
+
+t=0
+while t < 25:
+    new_ball()
+    t+=1
+print_drawn()
